b9249ba53e122d9a056ba333bd6e1d5907a29069
TonyStoynev <toni.stoinev@gmail.com>
2024-07-13 17:11:10 +0300

    Add `Semaphore` topic

---
+++ InterestingCSharpTopics/Semaphore/Program.cs
@@ -0,0 +1,33 @@
+﻿HttpClient _client = new()
+{
+    Timeout = TimeSpan.FromSeconds(5)
+};
+SemaphoreSlim _gate = new(10);
+
+Task.WaitAll(CreateCalls().ToArray());
+
+IEnumerable<Task> CreateCalls()
+{
+    for (int i = 0; i < 500; i++)
+    {
+        yield return CallGoogleAsync();
+    }
+}
+ async Task CallGoogleAsync()
+{
+    try
+    {
+        await _gate.WaitAsync();
+        var response = await _client.GetAsync("https://google.com");
+        _gate.Release();
+        Console.WriteLine(response.StatusCode);
+    }
+    catch (Exception e)
+    {
+
+         Console.WriteLine(e.Message);
+    }
+}
+    
+
+
