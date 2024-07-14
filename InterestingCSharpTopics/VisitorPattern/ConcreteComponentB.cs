7d42b65c27becf3796e34a917544cd3a0159391e
TonyStoynev <toni.stoinev@gmail.com>
2024-07-14 11:47:50 +0300

    Add `Visitor` design pattern

---
+++ InterestingCSharpTopics/VisitorPattern/ConcreteComponentB.cs
@@ -0,0 +1,20 @@
+﻿namespace VisitorPattern;
+
+// Each Concrete Component must implement the `Accept` method in such a way
+// that it calls the visitor's method corresponding to the component's
+// class.
+public class ConcreteComponentB : IComponent
+{
+    public void Accept(IVisitor visitor)
+    {
+        visitor.VisitConcreteComponentB(this);
+    }
+
+    // Concrete Components may have special methods that don't exist in
+    // their base class or interface. The Visitor is still able to use these
+    // methods since it's aware of the component's concrete class.
+    public string SpecialMethodOfConcreteComponentB()
+    {
+        return "B";
+    }
+}
---
+++ InterestingCSharpTopics/VisitorPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using VisitorPattern;
+
+List<IComponent> components = new List<IComponent>
+            {
+                new ConcreteComponentA(),
+                new ConcreteComponentB()
+            };
+
+Console.WriteLine("The client code works with all visitors via the base Visitor interface:");
+var visitor1 = new ConcreteVisitor1();
+ClientCode(components, visitor1);
+
+Console.WriteLine();
+
+Console.WriteLine("It allows the same client code to work with different types of visitors:");
+var visitor2 = new ConcreteVisitor2();
+ClientCode(components, visitor2);
+
+static void ClientCode(List<IComponent> components, IVisitor visitor)
+{
+    foreach (var component in components)
+    {
+        component.Accept(visitor);
+    }
+}
